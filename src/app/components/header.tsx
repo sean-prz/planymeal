10066d315d618e976121c64a73097f87f69a96c0
Perazzolo Sean <sean.perazzolo@gmail.com>
2025-05-11 21:38:00 +0200

    feat: shpping card icon

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,5 +1,6 @@
 "use client"
 import React from "react";
+import { ShoppingCartIcon } from '@heroicons/react/24/solid';
 import { useRouter } from "next/navigation"; 
 
 const Header = () => {
@@ -10,7 +11,11 @@ const Header = () => {
 
             <p onClick={() => route.push("/")}className=" m-0 text-xl flex-1 font-bold cursor-pointer ">planymeal</p>
             {/* This is a non-functional header. No links or logic here. */}
-            <div onClick={(e) => {e.stopPropagation;route.push("/shopping")}}className="flex-1 text-center cursor-pointer underline"> Shopping List </div>
+            <div onClick={(e) => {e.stopPropagation;route.push("/shopping")}}className="flex-1 text-center cursor-pointer underline flex justify-center items-center "> 
+                  
+               <p> Shopping List</p>
+          <ShoppingCartIcon className="ml-1 h-5 w-5 text-gray-500" aria-hidden="true" />
+            </div>
         </header>
     );
 };
